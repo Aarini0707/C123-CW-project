447ede195a0f4a31bcf92749bc698d7a181b13d7
Aarini0707 <79413058+Aarini0707@users.noreply.github.com>
2021-09-29 22:39:23 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+nosex=0;
+nosey=0;
+difference=0;
+leftWristX=0;
+rightWristX=0;
+
 function setup(){
     canvas= createCanvas(550,500);
     video= createCapture(VIDEO);
@@ -9,6 +15,10 @@ function setup(){
 
 function draw(){
     background('#fafa7a');
+    document.getElementById("size").innerHTML="Width & Height of the square= "+difference+"px";
+    fill('#fc8414');
+    stroke('#9c4b00');
+    square(nosex,nosey,difference);
 }
 
 function modelLoaded(){
@@ -18,5 +28,12 @@ function modelLoaded(){
 function gotposes(results){
     if(results.length>0){
         console.log(results);
+        nosex=results[0].pose.nose.x;
+        nosey=results[0].pose.nose.y;
+        console.log("Nose X= "+nosex+" Nose Y= "+nosey);
+        leftWristX=results[0].pose.leftWrist.x;
+        rightWristX=results[0].pose.rightWrist.x;
+        difference=floor(leftWristX-rightWristX);
+        console.log("Left Wrist X= "+leftWristX+" Right Wrist X= "+rightWristX+" Difference= "+difference);
     }
 }
